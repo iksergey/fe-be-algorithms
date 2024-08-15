44ce97ca34c1e4992293e03185b863db9e0f1c43
iksergey <i@ksergey.ru>
2024-08-15 13:22:19 +0300

    magic string

--- lesson-14/index.js
+++ lesson-14/index.js
@@ -39,3 +39,65 @@ console.log(check2([1, 2, 2, 1]));
 console.log(check2([1, 3, 2, 1]));
 console.log(check2([]));
 
+
+var isPalindrome1 = function (s) {
+    const cleanStr = s.toLowerCase().replace(/[^a-z0-9]/g, ''); // O(2n) -> O(n)
+    const newStr = cleanStr.split('').reverse().join(''); // O(2.5n) -> O(n)
+    return cleanStr === newStr; // O(n)
+};
+
+function isPalindrome2(s) {
+    s = s.toLowerCase().replace(/[^a-z0-9]/g, '')
+
+    let left = 0;
+    let right = s.length - 1;
+
+    while (left < right) {
+        if (s[left] !== s[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+
+    return true;
+}
+
+
+let str = "amanaplanacanalpanama";
+
+
+// split('')
+const mySplit = (s) => {
+    const arr = [];
+    for (let index = 0; index < s.length; index++) {
+        arr.push(s[index]);
+    }
+    return arr;
+}
+
+// reverse()
+const myReverse = (arr) => {
+    for (let index = 0; index < Math.floor(arr.length / 2); index++) {
+        const temp = arr[index];
+        arr[index] = arr[arr.length - 1 - index];
+        arr[arr.length - 1 - index] = temp;
+    }
+    return arr;
+}
+
+// join('')
+const myJoin = (arr) => {
+    let result = '';
+    for (let index = 0; index < arr.length; index++) {
+        const temp = arr[index];
+        result = result + temp;
+    }
+    return result;
+}
+
+const s1 = "12343";
+const s2 = myJoin(myReverse(mySplit(s1)));
+
+console.log(s1);
+console.log(s2);
